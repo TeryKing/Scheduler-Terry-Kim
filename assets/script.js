93c0ad7182863ce135a3b6922961dae06a95de65
TeryKing <tery_x3@hotmail.com>
2022-08-03 21:03:13 -0500

    Rough Draft, added timeblocks

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var today = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(today);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+})
